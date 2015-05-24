5116056dfac9570a154a6280a72c12ee3e528a67
ulrich <ulrich.atz@theodi.org>
2015-05-24 09:36:03 +0100

    Some more exploration

--- gh-wine-cellar-exlporation.R
+++ gh-wine-cellar-exlporation.R
@@ -26,9 +26,24 @@ sapply(wine, class)
 # Hmmmm
 
 options(stringsAsFactors = FALSE)
-wine <- read.csv(URL)
+wine <- read.csv.ssl(URL)
 
 sapply(wine, class)
 
+# Shouldn't consumption be numeric?!
+# We can force it - but what happens is a mystery
+# THIS IS BAD!
+wine$Consumption.April.2013...March.2014 <- as.numeric(wine$Consumption.April.2013...March.2014) 
+# Abort, abort
+
+wine_original <- read.csv.ssl(URL)
+wine <- wine_original # copies the data, no more waiting for downloading
+
+
+
+
+# Table with missings
+# as default: http://stackoverflow.com/questions/21724212/set-r-to-include-missing-data-how-can-is-set-the-usena-ifany-option-for-t
+table(wine[, 4], useNA = 'ifany')
 
 
