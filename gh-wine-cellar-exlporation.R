868d060eb584c64940c0f42084813cb181db44b9
ulrich <ulrich.atz@theodi.org>
2015-05-24 14:55:20 +0100

    Good stuff

--- gh-wine-cellar-exlporation.R
+++ gh-wine-cellar-exlporation.R
@@ -3,7 +3,7 @@ URL <- 'https://www.gov.uk/government/uploads/system/uploads/attachment_data/fil
 # NOT WORKING - but why
 wine <- read.csv(URL)
 
-# Introducing a function
+# Introducing a function - or maybe not
 
 read.csv.ssl <- function(url, ...){
   tmpfile <- tempfile()
@@ -16,7 +16,7 @@ read.csv.ssl <- function(url, ...){
 wine <- read.csv.ssl(URL)
 
 ######----------------------------------------
-# Now the questions: is it 'machine-readile'? 
+# Now the questions: is it 'machine-readable'? 
 
 # Encoding errors in Product.Name
 wine[1, ]
@@ -26,24 +26,60 @@ sapply(wine, class)
 
 # Hmmmm
 
-options(stringsAsFactors = FALSE)
+options(stringsAsFactors = FALSE) # TRUE is a terrible R default
 wine <- read.csv.ssl(URL)
 
 sapply(wine, class)
 
-# Shouldn't consumption be numeric?!
+# Shouldn't vintage and consumption be numeric?!
 # We can force it - but what happens is a mystery
+# There is a warning
 # THIS IS BAD!
-wine$Consumption.April.2013...March.2014 <- as.numeric(wine$Consumption.April.2013...March.2014) 
+
+wine$Vintage <- as.numeric(wine$Vintage) 
 # Abort, abort
 
 wine_original <- read.csv.ssl(URL)
 wine <- wine_original # copies the data, no more waiting for downloading
 
+
+# We know that a year should have four digits
+# Here are four ways to do it. They have different advantages or drawbacks.
+# You should've seen all of them
+
+# wine$Vintage[nchar(wine$Vintage) != 4]
+# wine[nchar(wine[, 2]) != 4, 2]
+# wine[nchar(wine[, "Vintage"]) != 4, "Vintage"]
+subset(wine, nchar(Vintage) != 4, select = Vintage)
+
+# Well...
+# NV = no value? Probably, but we're guessing. Which is also BAD. Heyho.
+wine$Vintage[wine$Vintage == "NV"] <- NA
+wine$Vintage[wine$Vintage == ""] <- NA
+
+wine$Vintage <- as.numeric(wine$Vintage)
+
+
+# We could go further and import them as dates
+# However, it needs a little hack to set as dates
+# Working with dates = NIGHTMARES GUARANTEED
+as.Date(paste0(wine$Vintage[1:5], "-01-01"))
+# Plus nice function name: dot AND camelCase
+
+# Now stuff like this is more meaningful
+summary(wine$Vintage)
+
+# Makes minimal sense
+hist(wine$Vintage)
+
+
+# Sooooo
 # So what's up with Consumption?
 wine$Consumption.April.2013...March.2014[nchar(wine$Consumption.April.2013...March.2014) > 3]
 
 # Aha! Aha... :(
+
+# We could replace them as before with custom assignments, but let's use a power tool
 
 
 
